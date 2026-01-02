e399cd14332b76c63e61588fbe80b827018dbc1e
Thiago Pederzolli M da Silva <thiagopederzolli@gmail.com>
2026-01-01 23:07:18 -0300

    feat: create the submitted sucess component and update the checkout modal to display after form submission

--- food-order-app/src/components/CheckoutModal.jsx
+++ food-order-app/src/components/CheckoutModal.jsx
@@ -1,10 +1,12 @@
 import { forwardRef, useImperativeHandle, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
 import CheckoutForm from './CheckoutForm';
+import Submitted from './Submitted';
 
 const CheckoutModal = forwardRef(function Modal({ title }, ref) {
   const dialog = useRef();
   const [formKey, setFormKey] = useState(0);
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   useImperativeHandle(ref, () => {
     return {
@@ -20,22 +22,31 @@ const CheckoutModal = forwardRef(function Modal({ title }, ref) {
   function handleClose() {
     setFormKey(prev => prev + 1);
     dialog.current.close();
+    setIsSubmitted(false);
   }
 
   function handleSuccessfulSubmit() {
     
     setFormKey(prev => prev + 1);
-    dialog.current.close();
+    setIsSubmitted(true);
+
   }
 
   function handleDialogClose() {
-    setFormKey(prev => prev + 1); 
+    setFormKey(prev => prev + 1);
+    setIsSubmitted(false);
   }
 
   return createPortal(
     <dialog className="modal" ref={dialog} onClose={handleDialogClose}>
-      <h2>{title}</h2>
-      <CheckoutForm key={formKey} onCloseModal={handleClose} onSuccess={handleSuccessfulSubmit} />
+      {isSubmitted ? (
+        <Submitted onCloseModal={handleClose} />
+      ): (
+        <>
+          <h2>{title}</h2>
+          <CheckoutForm key={formKey} onCloseModal={handleClose} onSuccess={handleSuccessfulSubmit} />
+        </>
+      )}
     </dialog>,
     document.getElementById('modal')
   );
